e62edae07368b1427520f04765d7ce9ac7a5bc31
ESTEVAN DADDY <enunez391@gmail.com>
2023-04-05 19:27:08 -0700

    added Comment.js

---
+++ models/comment.js
@@ -0,0 +1,48 @@
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        commentTxt: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dateCreated: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName:'comment'
+    }
+);
+
+    module.exports = Comment;
