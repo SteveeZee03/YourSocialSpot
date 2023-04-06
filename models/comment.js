a28e5ba176b3274366e9f2702a3360989e77ff7a
ESTEVAN DADDY <enunez391@gmail.com>
2023-04-05 19:57:56 -0700

    added Post.js

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class comment extends Model {}
+class Comment extends Model {}
 
 Comment.init(
     {
